38f96fc752bc3939c20679df93a0fc2410995cdb
takusan23 <takumu1200@gmail.com>
2026-01-14 00:37:17 +0900

    試し書きが残ってた

--- annotation/src/main/kotlin/io/github/takusan23/kspuistategenerator/annotation/GenerateUiState.kt
+++ annotation/src/main/kotlin/io/github/takusan23/kspuistategenerator/annotation/GenerateUiState.kt
@@ -1,8 +1,4 @@
 package io.github.takusan23.io.github.takusan23.kspuistategenerator.annotation
 
-import kotlin.reflect.KClass
-
 @Target(AnnotationTarget.CLASS)
 annotation class GenerateUiState
-
-annotation class ClassArg(val clazz: KClass<*>) // KSP ではクラスが存在しないエラーになってしまう
